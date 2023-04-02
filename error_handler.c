75bc5f2ae3f36df10ea6a6c58edd6739fc65f2b3
Barbara Menezes <32621812+barbara-menezes@users.noreply.github.com>
2023-04-02 19:04:29 -0300

    Add files via upload

---
+++ error_handler.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** error_handler.c
+** File description:
+** Error handler file
+*/
+
+#include "../include/my.h"
+
+int error_handler(void)
+{
+    write(2, "error - insuficient arguments", 30);
+    return 84;
+}
